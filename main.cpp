5f6c14156af775807ca61a26a5f04361c8769a48
Eduarda (Duda) Reolon <duda.reolon.1589@mail.linnbenton.edu>
2024-02-05 10:41:35 -0800

    code intro

---
+++ main.cpp
@@ -0,0 +1,4 @@
+// CS260 - Winter 2024 - Assignment 04 - Arbitrary list operations
+// Author: Eduarda (Duda) Reolon
+// Description: This program creates a linked-list that can insert and remove a value into a given position of the list.
+// This program can also return the value that is stored at a given position on the linked list without removing the value. 
