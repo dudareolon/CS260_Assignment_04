ae14a0e9ce6107b818e5c3fdb078df0e25fd9f57
Eduarda (Duda) Reolon <duda.reolon.1589@mail.linnbenton.edu>
2024-02-05 19:41:13 -0800

    node struct

--- main.cpp
+++ main.cpp
@@ -2,3 +2,12 @@
 // Author: Eduarda (Duda) Reolon
 // Description: This program creates a linked-list that can insert and remove a value into a given position of the list.
 // This program can also return the value that is stored at a given position on the linked list without removing the value. 
+
+
+#include <iostream>
+
+// The node struct defines what a 'data structure' node is
+struct Node {
+    int data; // a node is formed of a slot for the data
+    Node* next; // and a slot for the address of the next node
+};
